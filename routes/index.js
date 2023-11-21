019de63302a376cb95d5a5b2b37aa0e644220882
Liliana Herrera <122304941+llherrerap@users.noreply.github.com>
2023-11-20 19:45:54 -0500

    agregando y listando pacientes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
+// Archivo de enrutamientos
 var express = require('express');
 var router = express.Router();
 const { conexion } = require('../database/conexion')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Pagina hospital' });
 });
 
 //Enrutamiento para visualizar los medicos de la base de datos
@@ -16,6 +17,17 @@ router.get('/listado-medicos', (req, res) => {
     } else {
       console.log(resultado)
       res.status(200).render('medicos', { resultado })
+    }
+  })
+})
+
+router.get('/listado-pacientes', (req, res) => {
+  conexion.query('SELECT * FROM pacientes;', (error, resultado) => {
+    if (error) {
+      console.log('Ocurrio un error en la ejecución', error)
+      res.status(500).send('Error en la ejecución')
+    } else {
+      res.status(200).render('pacientes', { resultado })
     }
   })
 })
@@ -36,6 +48,24 @@ router.post('/agregar-medico', (req, res) => {
       res.status(500).send('Error en la ejecución')
     } else {
       res.status(200).redirect('/listado-medicos')
+    }
+  })
+})
+
+//Enrutamiento para agregar pacientes a la base de datos
+router.post('/agregar-paciente', (req, res) => {
+  const nombres = req.body.nombres
+  const apellidos = req.body.apellidos
+  const cedula = req.body.cedula
+  const fecha_nacimiento =req.body.fecha_nacimiento
+  const telefono = req.body.telefono
+  
+  conexion.query(`INSERT INTO pacientes (cedula, nombres, apellidos, fecha_nacimiento, telefono) VALUES (${cedula}, '${nombres}', '${apellidos}', '${fecha_nacimiento}', '${telefono}')`, (error, resultado) => {
+    if (error) {
+      console.log(error)
+      res.status(500).send('Ocurrio un error en la consulta')
+    } else {
+      res.status(200).redirect('/listado-pacientes')
     }
   })
 })
